c31bedb6b4e45643142259955556a102ffe8465a
Daniel Aguila <danielbaguila@gmail.com>
2023-08-14 11:13:17 +0900

    Can download video using youtube-dl but the model cannot find the path to the video

--- server.py
+++ server.py
@@ -1,5 +1,34 @@
+from __future__ import unicode_literals
+from fileinput import filename
+from pickle import FALSE
 from flask import Flask, render_template, request
+import youtube_dl
+import whisper
+import warnings
+
+warnings.filterwarnings("ignore")
+
 app = Flask(__name__)
+
+def save_to_mp3(url):
+    options={
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(options) as downloader:
+        downloader.download(["" + url + ""])
+    return downloader.prepare_filename(downloader.extract_info(url, download=False)).replace(".m4a",".mp3")
+
+def convert_file_to_text(file):
+    model = whisper.load_model("base")
+    result = model.transcribe(file, fp16=FALSE)
+    return result
+
 
 @app.route('/')
 def index():
@@ -8,8 +37,15 @@ def index():
 
 @app.route('/answer', methods=['POST'])
 def answer():
-    url_ = request.form.get("searchbox")
+    url_ = request.form.get('searchbox')
+    filename = save_to_mp3(url_)
+    result = convert_file_to_text(filename)
+    result_file = open('text_of_video.text', 'w')
+    result_file.write(result['text'])
+    result_file.close()
+
     answer_ = url_
+
     return(render_template('answer.html', answer=answer_))
 
 if __name__ == '__main__':
