35c67805a3e2131b441fd0112848fbef1170e7d4
Daniel Aguila <danielbaguila@gmail.com>
2023-08-14 09:22:06 +0900

    Tied the back end using Flask

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
-import cgi
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
-form = cgi.FieldStorage()
-searchVideo = form.getvalue('searchbox')
+@app.route('/')
+def index():
+    print("hello")
+    return(render_template('index.html'))
 
-print(searchVideo)
+@app.route('/answer', methods=['POST'])
+def answer():
+    url_ = request.form.get("searchbox")
+    answer_ = url_
+    return(render_template('answer.html', answer=answer_))
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8080, debug=True)
