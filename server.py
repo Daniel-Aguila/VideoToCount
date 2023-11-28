773fc6898ec3da51bc9271aee2eb52e47a3e88a9
Daniel Aguila <danielbaguila@gmail.com>
2023-11-28 08:52:02 -0600

    Implemneted the pipeline from transformers zero-shot

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 from __future__ import unicode_literals
-import json
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import tensorflow as tf
+import torch
+import transformers
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
 
 from sklearn.model_selection import train_test_split
 
 pd.set_option('display.max_colwidth',0)
-
-from concurrent.futures import process
 from pickle import FALSE
 from xml.dom import WRONG_DOCUMENT_ERR
 from flask import Flask, render_template, request
@@ -50,31 +50,37 @@ def convert_file_to_text(file):
     result = model.transcribe(file, fp16=FALSE)
     return result
 
-def preprocessAndWrite(text_,nameOfSong):
-    full_name_of_text = nameOfSong + ' text_of_video.text'
-    unprocessed_text = text_['text']
+#def preprocessAndWrite(text_,nameOfSong):
+    #full_name_of_text = nameOfSong + ' text_of_video.text'
+    #unprocessed_text = text_['text']
 
     #Preprocess
 
-    sentences = sent_tokenize(unprocessed_text)
-    #stemming
-    stemmer = PorterStemmer()
-    lemmatizer = WordNetLemmatizer()
+    #sentences = sent_tokenize(unprocessed_text)
+    #lemmatizer = WordNetLemmatizer()
 
-    for i in range(len(sentences)):
-        processed_text = re.sub('[^a-zA-Z]',' ', sentences[i]) #replaces all the characters except for a-z A-Z characters into spaces
-        processed_text = processed_text.lower()
-        processed_text = processed_text.split()
-        tokens_without_sw = [lemmatizer.lemmatize(word) for word in processed_text if word not in set(stopwords.words('english'))]
-    
-        sentences[i] = (" ").join(tokens_without_sw)
+    #for i in range(len(sentences)):
+    #    processed_text = re.sub('[^a-zA-Z]',' ', sentences[i]) #replaces all the characters except for a-z A-Z characters into spaces
+    #    processed_text = processed_text.lower()
+    #    processed_text = processed_text.split('.')
+    #    tokens_without_sw = [lemmatizer.lemmatize(word) for word in processed_text if word not in set(stopwords.words('english'))]
+    #
+    #    sentences[i] = (" ").join(tokens_without_sw)
 
-    result_file = open(full_name_of_text, 'w')
-    result_file.writelines(sentences)
-    print(sentences)
-    result_file.close()
-    return full_name_of_text
-    
+    #result_file = open(full_name_of_text, 'w')
+    #result_file.writelines(unprocessed_text)
+    #print(unprocessed_text)
+    #result_file.close()
+    #return full_name_of_text
+def topGenre(songLyrics):
+    classifier = pipeline("zero-shot-classification")
+    resultdict = classifier(songLyrics['text'], 
+        candidate_labels=[
+        "Romance", "Melancholy", "Happiness","Empowerment","Nostalgia","Peace","Excitement","Hope","Reflection","Euphoria"
+        ])
+    genre = resultdict['labels'][0]
+
+    return genre
 
 @app.route('/')
 def index():
@@ -86,10 +92,10 @@ def answer():
     url_ = request.form.get('searchbox')
     filename_, songname_ = save_to_mp3(url_)
     text = convert_file_to_text(filename_)
-    preprocessAndWrite(text,songname_)
+    songgenre_ = topGenre(text)
 
 
-    return(render_template('answer.html', songname=songname_))
+    return(render_template('answer.html', songname=songname_, songgenre=songgenre_))
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
